90d933c6fcc9b94ddbe217c2a646cf68396c404c
Ritika-Sai <jutursairitika@gmail.com>
2025-04-29 23:56:23 +0530

    Update React login module

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Login from './Login';
+import Signup from './Signup';
 
 function App() {
+  const [showLogin, setShowLogin] = useState(true);
+
+  const switchToSignup = () => {
+    setShowLogin(false);
+  };
+
+  const switchToLogin = () => {
+    setShowLogin(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {showLogin ? (
+        <Login switchToSignup={switchToSignup} />
+      ) : (
+        <Signup switchToLogin={switchToLogin} />
+      )}
     </div>
   );
 }
